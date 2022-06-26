de18dd37aae53da989a2d97295fbcc1f7a203fca
Sven Boor <sven.boor@gmail.com>
2022-06-26 23:30:04 +0200

    Bugfix.

--- zones/no_parking.py
+++ zones/no_parking.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 
 class NoParking(BaseModel):
-    start_date: Optional[datetime] = Field(default_factory=datetime.now().astimezone())
+    start_date: Optional[datetime] = Field(default_factory=lambda: datetime.now().astimezone())
     end_date: Optional[datetime]
