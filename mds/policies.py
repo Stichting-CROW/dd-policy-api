3398830655070e7a7ea9a9e698573477944d8443
Sven Boor <sven.boor@gmail.com>
2022-06-27 09:31:24 +0200

    Fixed timestamp.

--- mds/policies.py
+++ mds/policies.py
@@ -2,6 +2,7 @@ from sre_constants import OP_IGNORE
 from db_helper import db_helper
 from fastapi import HTTPException
 import time
+from datetime import datetime, timezone
 
 from pydantic import BaseModel
 from typing import List
@@ -14,6 +15,11 @@ class MDSPolicies(BaseModel):
     version: str = "1.2.0"
     updated: int
     data: PoliciesData
+
+    class Config:
+        json_encoders = {
+            datetime: lambda v: int(v.replace(tzinfo=timezone.utc).timestamp() * 1000),
+        }
 
 def get_policies(municipality):
     with db_helper.get_resource() as (cur, _):
--- mds/policy.py
+++ mds/policy.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, parse_obj_as
 import datetime
 from uuid import UUID, uuid1
 import json
-from datetime import timezone
+from datetime import timezone, datetime
 
 class Rule(BaseModel):
     name: str
@@ -18,29 +18,29 @@ class Rule(BaseModel):
 
 class Policy(BaseModel):
     policy_id: UUID = Field(default_factory=uuid1)
-    start_date: datetime.datetime
-    end_date: Optional[datetime.datetime]
-    published_date: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now().astimezone())
+    start_date: datetime
+    end_date: Optional[datetime]
+    published_date: datetime = Field(default_factory=lambda: datetime.now().astimezone())
     name: str
     description: str
     rules: List[Rule]
     municipality: str
+    
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.replace(tzinfo=timezone.utc).timestamp() * 1000,
+        }
 
 def convert_policy_row(row):
     # rules_json = json.loads(row["rules"])
     rules = parse_obj_as(List[Rule], row["rules"])
     return Policy(
         policy_id=row["policy_id"],
-        start_date=convert_datetime_to_millis(row["start_date"]),
-        end_date=convert_datetime_to_millis(row["end_date"]),
-        published_date=convert_datetime_to_millis(row["published_date"]),
+        start_date=row["start_date"],
+        end_date=row["end_date"],
+        published_date=row["published_date"],
         rules=rules,
         name=row["name"],
         description=row["description"],
         municipality=row["gm_code"]
     )
-
-def convert_datetime_to_millis(dt):
-    if dt == None:
-        return None
-    return dt.replace(tzinfo=timezone.utc).timestamp() * 1000
